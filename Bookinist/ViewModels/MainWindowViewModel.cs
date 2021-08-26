608d228bd022272b9379a6e7400de7be76b79fd0
ska <shvilpo@gmail.com>
2021-08-26 15:51:42 +0300

    MainViewModel, locator

---
+++ Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок 
+        private string _Title = "Main window";
+        public string Title { get => Title; set => Set(ref _Title, value); }
+        #endregion
+    }
+}
